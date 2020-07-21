4e87575e6a8b5533c49e3f232c97b6b0cf9d3cbe
Santiago Bernal G <santiagobernal3@gmail.com>
2020-07-21 09:55:21 -0500

    Avance

--- js/lib.js
+++ js/lib.js
@@ -94,6 +94,83 @@ $(document).ready(function() {
         }
     });
 
+    //menu vertical 1
+    $(document).ready(function(){
+        $("#btn-edutec").click(function(){
+            $('.event').hide(500,"swing");
+            $('data-event').slideDown(500,"swing");
+        });
+        $("#btn-integratic").click(function(){
+            $('.event').hide(500,"swing");
+            $('.integratic').slideDown(500,"swing");
+        });
+        $("#btn-edunovatic").click(function(){
+            $('.event').hide(500,"swing");
+            $('.edunovatic').slideDown(500,"swing");
+        });
+        $("#btn-foro1").click(function(){
+            $('.event').hide(500,"swing");
+            $('.foro1').slideDown(500,"swing");
+        });
+        $("#btn-foro2").click(function(){
+            $('.event').hide(500,"swing");
+            $('.foro2').slideDown(500,"swing");
+        });
+        $("#btn-foro3").click(function(){
+            $('.event').hide(500,"swing");
+            $('.foro3').slideDown(500,"swing");
+        });
+    });
+
+    //menu vertical 2
+    $(document).ready(function(){
+        $("#btn-alex").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.alex').slideDown(500,"swing");
+            
+        });
+        $("#btn-catalina").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.catalina').slideDown(500,"swing");
+        });
+        $("#btn-diana").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.diana').slideDown(500,"swing");
+        });
+        $("#btn-jake").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.jake').slideDown(500,"swing");
+        });
+        $("#btn-jas").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.jas').slideDown(500,"swing");
+        });
+        $("#btn-jorge").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.jorge').slideDown(500,"swing");            
+        });
+        $("#btn-lili").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.lili').slideDown(500,"swing");
+        });
+        $("#btn-marga").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.marga').slideDown(500,"swing");
+        });
+        $("#btn-tati").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.tati').slideDown(500,"swing");
+        });
+        $("#btn-jorgej").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.jorgej').slideDown(500,"swing");
+        });
+        $("#btn-luis").click(function(){
+            $('.exp').hide(500,"swing");
+            $('.luis').slideDown(500,"swing");
+        });
+    });
+
 //Menu de navegación
     $('[data-offset="true"]').each(function(){
         var $this = $(this);
